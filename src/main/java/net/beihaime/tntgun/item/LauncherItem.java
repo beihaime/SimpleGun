43de07baa7935f48d5605063f8ff57f5a2b23a85
beihaime <beihaime@outlook.com>
2026-08-25 21:22:19 +0800

    feat: add shoot tnt function

--- src/main/java/net/beihaime/tntgun/item/LauncherItem.java
+++ src/main/java/net/beihaime/tntgun/item/LauncherItem.java
@@ -2,11 +2,13 @@ package net.beihaime.tntgun.item;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class LauncherItem extends Item {
     public LauncherItem(Properties properties) {
@@ -14,10 +16,18 @@ public class LauncherItem extends Item {
 
     }
     public void fire(Player player) {
+        Vec3 look = player.getLookAngle();
         Level level = player.level();
-        Snowball snowball = new Snowball(level,player);
-        snowball.setPos(player.getX(), player.getY(), player.getZ());
-        snowball.shootFromRotation(player,player.getXRot(),player.getYRot(),0,4F,0);
-        level.addFreshEntity(snowball);
+        PrimedTnt tnt = new PrimedTnt(
+                level,player.getX() ,
+                player.getEyeY(),
+                player.getZ(),
+                player);
+        tnt.setDeltaMovement(
+                look.x * 4,
+                look.y * 1.5,
+                look.z * 4
+        );
+        level.addFreshEntity(tnt);
     }
 }
